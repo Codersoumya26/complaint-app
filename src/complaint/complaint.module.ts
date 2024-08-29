4c99f921393ccdab0236e30a51644ca12db1d839
Codersoumya26 <panjasoumya2@gmail.com>
2024-08-29 16:53:01 +0530

    Add controller via nest generate controller complaint

--- src/complaint/complaint.module.ts
+++ src/complaint/complaint.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ComplaintService } from './complaint.service';
+import { ComplaintController } from './complaint.controller';
 
 @Module({
-  providers: [ComplaintService]
+  providers: [ComplaintService],
+  controllers: [ComplaintController]
 })
 export class ComplaintModule {}
